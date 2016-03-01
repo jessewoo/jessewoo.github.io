fc2988444e6a11f37d8b88ba607c0a13f0d8c2ef
Jesse Woo <woojesse@gmail.com>
2016-02-29 21:54:14 -0800

    Bootstrapped site

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<div class="container">
+		<p class="text-muted">
+			<i class="fa fa-twitter-square"></i> Twitter: <a href="https://twitter.com/rcsbpdb" target="_blank">Twitter</a> -
+			<i class="fa fa-github-square"></i> GitHub: <a href="https://github.com/rcsb" target="_blank">GitHub</a>
+		</p>
+	</div>
+</footer>
