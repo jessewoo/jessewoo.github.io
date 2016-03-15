9f70ccaafbbbf8c9e899830e10ec3d45f76ed0ee
Jesse Woo <woojesse@gmail.com>
2016-03-14 23:04:55 -0700

    Pushed to new theme

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-	<div class="container">
-		<p class="text-muted">
-			<i class="fa fa-twitter-square"></i> Twitter: <a href="https://twitter.com/rcsbpdb" target="_blank">Twitter</a> -
-			<i class="fa fa-github-square"></i> GitHub: <a href="https://github.com/rcsb" target="_blank">GitHub</a>
-		</p>
-	</div>
-</footer>
